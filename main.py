343770dcbc6ea77e8dec4bc2204afb7f4994750c
Mz-0075 <134430337+Mz-0075@users.noreply.github.com>
2025-04-12 00:38:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# app.py
+import streamlit as st
+
+st.title("4G Cell File Generator")
+
+# User inputs
+lat = st.text_input("Enter Latitude (LAT)")
+lon = st.text_input("Enter Longitude (LON)")
+azi1 = st.text_input("Enter First Azimuth")
+azi2 = st.text_input("Enter Second Azimuth")
+azi3 = st.text_input("Enter Third Azimuth")
+
+# Predefined values
+system = "2100"
+site = "KER32915"
+cell_name = "461150_1"
+cell_id = "1"
+pci = "1"
+tac = "56238"
+arfcn = "1357"
+mcc = "404"
+mnc = "43"
+ant_bw = "65"
+
+# Generate TXT file when button is clicked
+if st.button("Generate 4G Cell TXT"):
+    header = "SYSTEM\tSITE\tLAT\tLON\tDIR\tCELL_NAME\tCELL_ID\tPCI\tTAC\tARFCN\tMCC\tMNC\tANT_BW"
+    row1 = f"{system}\t{site}\t{lat}\t{lon}\t{azi1}\t{cell_name}\t{cell_id}\t{pci}\t{tac}\t{arfcn}\t{mcc}\t{mnc}\t{ant_bw}"
+    row2 = f"{system}\t{site}\t{lat}\t{lon}\t{azi2}\t{cell_name}\t{cell_id}\t{pci}\t{tac}\t{arfcn}\t{mcc}\t{mnc}\t{ant_bw}"
+    row3 = f"{system}\t{site}\t{lat}\t{lon}\t{azi3}\t{cell_name}\t{cell_id}\t{pci}\t{tac}\t{arfcn}\t{mcc}\t{mnc}\t{ant_bw}"
+
+    output_text = header + "\n" + row1 + "\n" + row2 + "\n" + row3
+
+    st.download_button(
+        label="Download 4G_cell_output.txt",
+        data=output_text,
+        file_name="4G_cell_output.txt",
+        mime="text/plain"
+    )
